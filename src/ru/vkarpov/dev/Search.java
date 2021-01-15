0f368e527a7651d89877b4ef16bdcf6f49c60fd7
vvkarpov <chuzhoi36@gmail.com>
2021-01-15 19:23:19 +0300

    fix bugzz

--- src/ru/vkarpov/dev/Search.java
+++ src/ru/vkarpov/dev/Search.java
@@ -2,8 +2,8 @@ package ru.vkarpov.dev;
 
 public class Search {
     public static void main(String[] args) {
-        int[] array = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-        int num = 3;
+        int[] array = new int[] {1, 2, 3, 4, 5, 6};
+        int num = 2;
         boolean result = BinarySearch(array, num);
         System.out.println(result);
     }
@@ -11,23 +11,19 @@ public class Search {
     public static boolean BinarySearch(int[] array, int num){
         int low = 0;
         int high = array.length - 1;
-        int mid = (int) Math.ceil(high / 2.0);
-
-        while (true){
+        while (low <= high){
+            int mid = (low + high) / 2;
             if (num == array[mid]){
                 return true;
             }
-            else if (num < array[mid]){
-                high = mid;
-                mid = (int) Math.ceil(high / 2.0);
+            if (num < array[mid]){
+                high = mid - 1;
             }
-            else if (num > array[mid]){
-                low = mid;
-                mid = (int) Math.ceil((high - low) / 2.0);
+            else {
+                low = mid + 1;
             }
-            else
-                return false;
         }
+        return false;
     }
 
 }
